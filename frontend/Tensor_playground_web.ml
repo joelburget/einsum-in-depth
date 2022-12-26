4cfe9281595c5bd72728ce2521a7ba5df382ff20
Joel Burget <joelburget@gmail.com>
2022-12-25 18:13:51 -0800

    Add frontend changes.

--- frontend/Tensor_playground_web.ml
+++ frontend/Tensor_playground_web.ml
@@ -1,4 +1,13 @@
 open Brr
 open Tensor_playground
 
-let () = El.set_children (Document.body G.document) El.[ txt' "Hello World!" ]
+let render_elem = function
+  | Tensor_type.Elem.Concrete i -> El.txt' (Int.to_string i)
+  | Variable v -> El.txt' v
+
+let render_tensor_type t = El.txt' (Tensor_type.to_string t)
+
+let () =
+  let tensor_type = Tensor_type.[ Elem.Variable "a"; Elem.Concrete 1 ] in
+  let content = El.[ div [ render_tensor_type tensor_type ] ] in
+  El.set_children (Document.body G.document) content
