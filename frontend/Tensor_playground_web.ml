b477317763814164d43197e74079a66883111d20
Joel Burget <joelburget@gmail.com>
2022-12-28 14:24:16 -0800

    Checkpoint frontend before adding pretty-printing.
    `dune build` -> tensor_playground_web.bc.js (via `yarn webpack:analyze`):
    * Stat: 3.9 MB
    * Parsed: 1008.69 KB
    * Gzipped: 276.7 KB
    
    `dune build --profile=release` -> tensor_playground_web.bc.js (via `yarn webpack:analyze`):
    * Stat: 294.49 KB
    * Parsed: 260.15 KB
    * Gzipped: 79.74 KB
    
    Probably larger than I'd like due to Menhir.

--- frontend/Tensor_playground_web.ml
+++ frontend/Tensor_playground_web.ml
@@ -9,23 +9,52 @@ let render_elem = function
 
 let render_tensor_type t = El.txt' (Tensor_type.to_string t)
 
+let process_type_input str =
+  str |> Lexing.from_string |> Type_parser.ty Type_lexer.token
+
+module Parse_result = struct
+  type t = Ok of Tensor_type.t | No_input | Error of string
+end
+
+let parse str =
+  try Parse_result.Ok (process_type_input str) with
+  | Type_lexer.Eof ->
+      if str = "" then No_input else Error "Unknown error (unclosed bracket?)"
+  | Type_lexer.Error msg -> Error msg
+
+let explain : Tensor_type.t -> Tensor_type.t -> El.t list =
+ fun _rev_a _rev_b -> El.[ txt' "TODO" ]
+
+let update_output : (string * string) signal -> El.t list signal =
+  S.map (fun (a, b) ->
+      match (parse a, parse b) with
+      | Ok a, Ok b -> explain (List.rev a) (List.rev b)
+      | No_input, _ | _, No_input -> El.[ txt' "(empty input)" ]
+      | Error msg, Ok _ -> El.[ txt' "Error parsing A: "; txt' msg ]
+      | Ok _, Error msg -> El.[ txt' "Error parsing B: "; txt' msg ]
+      | Error msg1, Error msg2 ->
+          El.
+            [
+              div [ txt' "Error parsing A: "; txt' msg1 ];
+              div [ txt' "Error parsing B: "; txt' msg2 ];
+            ])
+
 let () =
   let a_input = El.input () in
-  let a_signal, a_send = E.create () in
   let b_input = El.input () in
-  (* let b_signal, b_send = E.create () in *)
+  let result_output = El.div [] in
+
+  let a_signal, set_a = S.create "" in
+  let b_signal, set_b = S.create "" in
+
   Brr_note.Evr.endless_listen (El.as_target a_input) Ev.change (fun _evt ->
-      let v = El.prop El.Prop.value a_input in
-      Console.log [ v ];
-      a_send v);
-  (* Brr_note.Evr.on_el Ev.change *)
-  (*   (fun _evt -> b_send (Jstr.to_string (El.prop El.Prop.value b_input))) *)
-  (*   b_input; *)
-  let result_output = El.span [] in
-  let a_signal =
-    S.hold Jstr.empty a_signal |> S.map (fun jstr -> [ El.txt jstr ])
-  in
-  Brr_note.Elr.def_children result_output a_signal;
+      set_a (Jstr.to_string (El.prop El.Prop.value a_input)));
+  Brr_note.Evr.endless_listen (El.as_target b_input) Ev.change (fun _evt ->
+      set_b (Jstr.to_string (El.prop El.Prop.value b_input)));
+
+  let output_signal = S.Pair.v a_signal b_signal |> update_output in
+  Brr_note.Elr.def_children result_output output_signal;
+
   El.set_children (Document.body G.document)
     El.
       [
