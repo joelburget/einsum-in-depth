e3501f6a78ad1ed976ab8bb1e59af9b669afddbc
Joel Burget <joelburget@gmail.com>
2023-04-10 09:34:59 -0700

    Remove ops which I don't care about.

--- lib/Einops.ml
+++ lib/Einops.ml
@@ -24,28 +24,3 @@ module Rewrite = struct
 
   let pp = Fmt.(box (pair ~sep:(any " -> ") Bindings.pp Group.pp))
 end
-
-module Op_kind = struct
-  type t = Rearrange | Reduce | Repeat
-
-  let pp ppf = function
-    | Rearrange -> Fmt.pf ppf "rearrange"
-    | Reduce -> Fmt.pf ppf "reduce"
-    | Repeat -> Fmt.pf ppf "repeat"
-end
-
-module Equation = struct
-  type t = string * int
-
-  let pp ppf (name, i) = Fmt.pf ppf "%s = %d" name i
-end
-
-module Op = struct
-  type t = Op of Op_kind.t * Rewrite.t * Equation.t list
-
-  let pp ppf (Op (op_kind, rewrite, eqns)) =
-    Fmt.(
-      pf ppf "Op @[(%a,@ %a,@ %a)@]" Op_kind.pp op_kind Rewrite.pp rewrite
-        (brackets (list ~sep:(any " ") Equation.pp))
-        eqns)
-end
