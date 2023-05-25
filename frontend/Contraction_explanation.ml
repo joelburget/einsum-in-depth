3bd79a7c56387a1ee622f813458f22700c0c9c2d
Joel Burget <joelburget@gmail.com>
2023-05-25 16:22:50 -0700

    Remove path stuff from Contraction_explanation.

--- frontend/Contraction_explanation.ml
+++ frontend/Contraction_explanation.ml
@@ -31,38 +31,12 @@ let rec get_names = function
 
 let list_of_err = function Result.Ok _ -> [] | Error err -> [ err ]
 
-let validate_path n_tensors path =
-  if List.length path = 0 then None
-  else if List.length path <> n_tensors - 1 then
-    Some [ fmt_txt "Path must have length (%d)" (n_tensors - 1) ]
-  else
-    let rec loop n_tensors path =
-      match path with
-      | [] -> None
-      | (x, y) :: path ->
-          if x < 0 || y < 0 then
-            Some [ txt' "All path components must be non-negative" ]
-          else if x >= n_tensors || y >= n_tensors then
-            Some
-              [
-                fmt_txt
-                  "Path components must be between 0 and %d (the number of \
-                   un-contracted tensors remaining at this point) (got %a)"
-                  (n_tensors - 1)
-                  Fmt.(parens (pair ~sep:comma int int))
-                  (x, y);
-              ]
-          else loop n_tensors path
-    in
-    loop n_tensors path
-
 let validate_inputs :
     int list ->
     int list ->
     Einops.Rewrite.t ->
-    (int * int) list ->
     (validated_inputs, El.t list) result =
- fun a_ty b_ty eqn _path ->
+ fun a_ty b_ty eqn ->
   let bindings, rhs = eqn in
   match bindings with
   | [ g1; g2 ] -> (
@@ -112,9 +86,9 @@ let validate_inputs :
       (* TODO: generalize *)
       Error [ txt' "Currently only binary contractions are supported" ]
 
-let explain_contraction : validated_inputs -> (int * int) list -> El.t list =
+let explain_contraction : validated_inputs -> El.t list =
  (* We ignore the path for now since it has to be [(0, 1)] *)
- fun { a_indices; b_indices; rhs_names } _path ->
+ fun { a_indices; b_indices; rhs_names } ->
   let a_names_set = a_indices |> List.map fst |> String_set.of_list in
   let b_names_set = b_indices |> List.map fst |> String_set.of_list in
   let rhs_names_set = String_set.of_list rhs_names in
@@ -214,12 +188,6 @@ let explain_contraction : validated_inputs -> (int * int) list -> El.t list =
       ];
   ]
 
-let parse_contraction : string -> (Einops.Rewrite.t, string * string) result =
- fun str ->
-  match parse_einsum str with
-  | Ok einsum -> Ok einsum
-  | Error (msg1, msg2) -> Error (msg1, msg2)
-
 let explain container a_type_str b_type_str contraction_str =
   let result_output = div [] in
   let parsed_a_signal, a_input, a_err_elem =
@@ -229,29 +197,20 @@ let explain container a_type_str b_type_str contraction_str =
     bracketed_parsed_input parse_type b_type_str
   in
   let contraction_signal, c_input, c_err_elem =
-    parsed_input parse_contraction contraction_str
-  in
-  let parse_path s =
-    s |> Path_parser.parse |> Result.map_error (fun msg -> (msg, ""))
-  in
-  let parsed_path_signal, path_input, path_err_elem =
-    bracketed_parsed_input parse_path ""
+    parsed_input parse_einsum contraction_str
   in
 
-  let f as_opt bs_opt contraction_opt parsed_path_opt =
-    match (as_opt, bs_opt, contraction_opt, parsed_path_opt) with
-    | Some as_, Some bs, Some contraction, Some path -> (
-        match validate_path 2 path with
-        | Some err -> err
-        | None -> (
-            match validate_inputs as_ bs contraction path with
-            | Ok validated_inputs -> explain_contraction validated_inputs path
-            | Error err -> err))
+  let f as_opt bs_opt contraction_opt =
+    match (as_opt, bs_opt, contraction_opt) with
+    | Some as_, Some bs, Some contraction -> (
+        match validate_inputs as_ bs contraction with
+        | Ok validated_inputs -> explain_contraction validated_inputs
+        | Error err -> err)
     | _ -> [ txt' "TODO" ]
   in
 
   let explanation_signal =
-    l4 f parsed_a_signal parsed_b_signal contraction_signal parsed_path_signal
+    S.l3 f parsed_a_signal parsed_b_signal contraction_signal
   in
 
   Elr.def_children result_output explanation_signal;
@@ -260,6 +219,5 @@ let explain container a_type_str b_type_str contraction_str =
       div [ txt' "A: "; a_input; a_err_elem ];
       div [ txt' "B: "; b_input; b_err_elem ];
       div [ txt' "Contraction: "; c_input; c_err_elem ];
-      div [ txt' "Path: "; path_input; path_err_elem ];
       result_output;
     ]
