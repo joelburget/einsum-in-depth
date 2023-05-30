c9b8c86d6774fc4fbe2e5e91c58ca7c599ac6f1b
Joel Burget <joelburget@gmail.com>
2023-05-30 13:43:15 -0700

    Special case for 1-tuples.

--- lib/Path_parser.ml
+++ lib/Path_parser.ml
@@ -2,22 +2,26 @@ module Scanning = Scanf.Scanning
 
 type path = int list list
 
+let parse_int str =
+  try str |> String.trim |> int_of_string
+  with Failure _ ->
+    raise (Failure (Fmt.str "%S could not be parsed as an int" str))
+
+let is_space str = String.trim str = ""
+
 let parse_int_list s =
   try
     let int_list = Scanf.bscanf s "(%[^)])" (fun x -> x) in
     let int_list = String.split_on_char ',' int_list in
-    Ok
-      (List.map
-         (fun str ->
-           try str |> String.trim |> int_of_string
-           with Failure _ ->
-             raise (Failure (Fmt.str "%S could not be parsed as an int" str)))
-         int_list)
+    match int_list with
+    | [ x1; x2 ] when is_space x2 -> Ok [ parse_int x1 ]
+    | _ -> Ok (List.map parse_int int_list)
   with
   | Scanf.Scan_failure s -> Error s
   | Failure s -> Error ("Invalid list: " ^ s)
   | End_of_file -> Error "Unexpected end of file"
 
+(* TODO: "," then trim? *)
 let parse_sep s =
   try Scanf.bscanf s ", " () with Scanf.Scan_failure _ | End_of_file -> ()
 
@@ -42,10 +46,15 @@ let parse : string -> (path * Tensor_type.bracketed, string) result =
   |> Result.map (fun path ->
          (path, if brackets_stripped then Tensor_type.Bracketed else Unbracketed))
 
+let pp_int_list ppf xs =
+  match xs with
+  | [ x ] -> Fmt.pf ppf "%d," x
+  | _ -> Fmt.(list ~sep:comma int) ppf xs
+
 let%expect_test "parse_int_list" =
   let go s =
     match parse_int_list (Scanning.from_string s) with
-    | Ok xs -> Fmt.(pr "@[(%a)@]@." (list ~sep:comma int) xs)
+    | Ok xs -> Fmt.(pr "@[(%a)@]@." pp_int_list xs)
     | Error msg -> Fmt.pr "%s@." msg
   in
   go "(1, 2)";
@@ -53,13 +62,16 @@ let%expect_test "parse_int_list" =
   go "(1, a)";
   go "(1, 2.0)";
   go "(1,";
+  go "(1,)";
   [%expect
     {|
     (1, 2)
     Invalid list: "1 2" could not be parsed as an int
     Invalid list: " a" could not be parsed as an int
     Invalid list: " 2.0" could not be parsed as an int
-    Unexpected end of file |}]
+    Unexpected end of file
+    (1,)
+    |}]
 
 let%expect_test "parse" =
   let go s =
